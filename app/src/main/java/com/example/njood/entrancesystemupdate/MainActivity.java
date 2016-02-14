79a436a5debfc219e7c5a74a1e4fc5249eba755d
njoodnj <aidmenp@gmail.com>
2016-02-14 17:29:01 +0300

    login complete

--- app/src/main/java/com/example/njood/entrancesystemupdate/MainActivity.java
+++ app/src/main/java/com/example/njood/entrancesystemupdate/MainActivity.java
@@ -1,52 +1,111 @@
 package com.example.njood.entrancesystemupdate;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends Activity {
+
+    EditText id, password;
+    String Id, Password;
+    Context ctx=this;
+    String ID=null, NAME=null, PASSWORD=null, EMAIL=null, ADDRESS=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
+        id = (EditText) findViewById(R.id.NationalID);
+        password = (EditText) findViewById(R.id.passwordHP);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    public void main_register(View v){
+        startActivity(new Intent(this,register.class));
+    }
+
+    public void main_login(View v){
+        Id = id.getText().toString();
+        Password = password.getText().toString();
+        BackGround b = new BackGround();
+        b.execute(Id, Password);
+    }
+
+    class BackGround extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String id = params[0];
+            String password = params[1];
+            String data="";
+            int tmp;
+
+            try {
+                URL url = new URL("http://10.0.2.2/ES/login.php");
+                String urlParams = "r_id="+id+"&r_password="+password;
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                OutputStream os = httpURLConnection.getOutputStream();
+                os.write(urlParams.getBytes());
+                os.flush();
+                os.close();
+
+                InputStream is = httpURLConnection.getInputStream();
+                while((tmp=is.read())!=-1){
+                    data+= (char)tmp;
+                }
+
+                is.close();
+                httpURLConnection.disconnect();
+
+                return data;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(String s) {
+
+            try {
+                JSONObject root = new JSONObject(s);
+                JSONObject user_data = root.getJSONObject("user_data");
+                ID = user_data.getString("r_id");
+                NAME = user_data.getString("r_name");
+                PASSWORD = user_data.getString("r_password");
+                EMAIL = user_data.getString("r_email");
+                ADDRESS = user_data.getString("r_address");
+            } catch (JSONException e) {
+                e.printStackTrace();
+
+            }
+
+            Intent i = new Intent(ctx, User_Home.class);
+            i.putExtra("r_id", ID);
+            i.putExtra("r_name", NAME);
+            i.putExtra("r_password", PASSWORD);
+            i.putExtra("r_email", EMAIL);
+            i.putExtra("r_address", ADDRESS);
+            startActivity(i);
+
+        }
     }
 }
